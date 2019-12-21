8a57f062d98e3234a4a6c993569034755eb7a0c7
Ananda Umamil <zweimach@zweimach.org>
2019-12-22 06:00:25 +0700

    No reassign parameters

--- src/index.js
+++ src/index.js
@@ -111,8 +111,6 @@ function getRandomChar() {
 // get random integer between two values
 // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   //The maximum is inclusive and the minimum is inclusive
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
